dea539bfa1b7dc5f457f0874638bbdbe908a3d1b
Warren Wang <warren.wang2020@gmail.com>
2023-05-18 17:29:32 -0700

    added some endpoints and content, using chi

--- main.go
+++ main.go
@@ -1,9 +1,14 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
+	"strings"
+
+	"github.com/go-chi/chi"
 )
 
+// allows cross origin requests
 func middlewareCors(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Access-Control-Allow-Origin", "*")
@@ -17,17 +22,75 @@ func middlewareCors(next http.Handler) http.Handler {
 	})
 }
 
-func main() {
+// metrics - counting landing page server hits
+type apiConfig struct {
+	fileserverHits int
+}
 
-	// create a new serveMux
-	mux := http.NewServeMux()
-	corsMux := middlewareCors(mux)
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.HandlerFunc {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits++
+		next.ServeHTTP(w, r)
+	})
+}
 
-	srv := http.Server{
-		Addr:    ":8080",
-		Handler: corsMux,
+func (cfg *apiConfig) metricsHandlerFunc(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(200)
+	w.Write([]byte(fmt.Sprintf("Hits: %d", cfg.fileserverHits)))
+}
+
+// Assets fileserver
+// from: https://github.com/go-chi/chi/blob/master/_examples/fileserver/main.go
+// FileServer conveniently sets up a http.FileServer handler to serve
+// static files from a http.FileSystem.
+func FileServer(r chi.Router, path string, root http.FileSystem) {
+	if strings.ContainsAny(path, "{}*") {
+		panic("FileServer does not permit any URL parameters.")
 	}
 
-	srv.ListenAndServe()
+	if path != "/" && path[len(path)-1] != '/' {
+		r.Get(path, http.RedirectHandler(path+"/", 301).ServeHTTP)
+		path += "/"
+	}
+	path += "*"
 
+	r.Get(path, func(w http.ResponseWriter, r *http.Request) {
+		rctx := chi.RouteContext(r.Context())
+		pathPrefix := strings.TrimSuffix(rctx.RoutePattern(), "/*")
+		fs := http.StripPrefix(pathPrefix, http.FileServer(root))
+		fs.ServeHTTP(w, r)
+	})
+}
+
+// main
+func main() {
+	filepathRoot := "."
+	assetsDir := "assets"
+	apiCfg := &apiConfig{fileserverHits: 0}
+
+	// chi router -- use it to stop extra HTTP methods from working, restrict to GETs
+	r := chi.NewRouter()
+	r.Use(middlewareCors) // wrap with middleware allowing CORS
+
+	// main landing page, serves index.html
+	r.Get("/", apiCfg.middlewareMetricsInc(http.FileServer(http.Dir(filepathRoot))))
+
+	// readiness endpoint
+	r.Get("/healthz", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(200)
+		w.Write([]byte("OK"))
+	})
+
+	// metrics: number of serve requests
+	r.Get("/metrics", apiCfg.metricsHandlerFunc)
+
+	// assets
+	FileServer(r, "/assets", http.Dir(assetsDir))
+
+	// create server and listen
+	srv := http.Server{
+		Addr:    ":8080",
+		Handler: r,
+	}
+	srv.ListenAndServe()
 }

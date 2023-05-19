4c574511f73b5a415f548be96460f82ea42d7349
Warren Wang <warren.wang2020@gmail.com>
2023-05-19 15:59:53 -0700

    moved metrics api endpoint, added a validate_chirp api endpoint

--- main.go
+++ main.go
@@ -1,7 +1,11 @@
 package main
 
 import (
+	"encoding/json"
+	"errors"
 	"fmt"
+	"io/ioutil"
+	"log"
 	"net/http"
 	"strings"
 
@@ -35,62 +39,126 @@ func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.HandlerFunc {
 }
 
 func (cfg *apiConfig) metricsHandlerFunc(w http.ResponseWriter, r *http.Request) {
-	w.WriteHeader(200)
-	w.Write([]byte(fmt.Sprintf("Hits: %d", cfg.fileserverHits)))
+	w.Header().Set("Content-Typ", "text/html")
+	w.WriteHeader(http.StatusOK)
+	htmlTemplate, err := ioutil.ReadFile("./admin/metrics/template.html")
+	if err != nil {
+		log.Fatalf("received err: %v", err)
+	}
+	htmlData := fmt.Sprintf(string(htmlTemplate), cfg.fileserverHits)
+	w.Write([]byte(htmlData))
 }
 
-// Assets fileserver
-// from: https://github.com/go-chi/chi/blob/master/_examples/fileserver/main.go
-// FileServer conveniently sets up a http.FileServer handler to serve
-// static files from a http.FileSystem.
-func FileServer(r chi.Router, path string, root http.FileSystem) {
-	if strings.ContainsAny(path, "{}*") {
-		panic("FileServer does not permit any URL parameters.")
+// -------------
+
+type errorBody struct {
+	Error string `json:"error"`
+}
+
+// wrapper for respondWithJSON for sending errors as the interface used to be converted to json
+func respondWithError(w http.ResponseWriter, code int, err error) {
+	respondWithJSON(w, code, errorBody{Error: err.Error()})
+}
+
+// handles http requests and return json
+func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(code)
+	response, err := json.Marshal(payload)
+	if err != nil {
+		log.Fatal(err)
+	}
+	w.Write(response)
+}
+
+// censors bad words from a string by replacing them with some censor
+// listOfBadWords is a list of strings of bad words to censor
+// s is the string to sensor
+// badWordReplacement is the censor string (replacement val)
+func censorChirp(listOfBadWords []string, s, badWordReplacement string) string {
+	chirpWords := strings.Split(s, " ")
+	for i, word := range chirpWords {
+		for _, badWord := range listOfBadWords {
+			if strings.ToLower(word) == badWord {
+				chirpWords[i] = badWordReplacement
+			}
+		}
+	}
+	return strings.Join(chirpWords, " ")
+}
+
+// validate new chirps
+func validateChirpHandler(w http.ResponseWriter, r *http.Request) {
+	type parameters struct {
+		Body string `json:"body"`
 	}
 
-	if path != "/" && path[len(path)-1] != '/' {
-		r.Get(path, http.RedirectHandler(path+"/", 301).ServeHTTP)
-		path += "/"
+	type returnVals struct {
+		Cleaned_body string `json:"cleaned_body"`
 	}
-	path += "*"
 
-	r.Get(path, func(w http.ResponseWriter, r *http.Request) {
-		rctx := chi.RouteContext(r.Context())
-		pathPrefix := strings.TrimSuffix(rctx.RoutePattern(), "/*")
-		fs := http.StripPrefix(pathPrefix, http.FileServer(root))
-		fs.ServeHTTP(w, r)
-	})
+	// decode the chirp from JSON into go struct
+	decoder := json.NewDecoder(r.Body)
+	params := parameters{}
+	err := decoder.Decode(&params)
+	if err != nil {
+		// an error will be thrown if the JSON is invalid or has the wrong types
+		// any missing fields will simply have their values in the struct set to their zero value
+		respondWithError(w, http.StatusInternalServerError, errors.New("Something went wrong"))
+		return
+	}
+
+	// check if chirp is too long
+	if len(params.Body) > 140 {
+		respondWithError(w, http.StatusBadRequest, errors.New("Chirp is too long"))
+		return
+	}
+
+	// reply with censored chirp
+	badWordReplacement := "****"
+	listOfBadWords := []string{"kerfuffle", "sharbert", "fornax"}
+	cleanedChirp := censorChirp(listOfBadWords, params.Body, badWordReplacement)
+	respondWithJSON(w, http.StatusOK, returnVals{Cleaned_body: cleanedChirp})
 }
 
 // main
 func main() {
 	filepathRoot := "."
-	assetsDir := "assets"
 	apiCfg := &apiConfig{fileserverHits: 0}
 
 	// chi router -- use it to stop extra HTTP methods from working, restrict to GETs
 	r := chi.NewRouter()
-	r.Use(middlewareCors) // wrap with middleware allowing CORS
+	r.Mount("/", apiCfg.middlewareMetricsInc(http.FileServer(http.Dir(filepathRoot))))
 
-	// main landing page, serves index.html
-	r.Get("/", apiCfg.middlewareMetricsInc(http.FileServer(http.Dir(filepathRoot))))
+	// ------------ api ---------------
+	// api router
+	apiRouter := chi.NewRouter()
+	r.Mount("/api", apiRouter)
 
 	// readiness endpoint
-	r.Get("/healthz", func(w http.ResponseWriter, r *http.Request) {
+	apiRouter.Get("/healthz", func(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(200)
 		w.Write([]byte("OK"))
 	})
 
-	// metrics: number of serve requests
-	r.Get("/metrics", apiCfg.metricsHandlerFunc)
+	apiRouter.Post("/validate_chirp", validateChirpHandler)
 
-	// assets
-	FileServer(r, "/assets", http.Dir(assetsDir))
+	// ------------ api ---------------
+
+	// admin
+	adminRouter := chi.NewRouter()
+	r.Mount("/admin", adminRouter)
+
+	// metrics: number of serve requests
+	adminRouter.Get("/metrics", apiCfg.metricsHandlerFunc)
+
+	// wrap the main chi router with a handler function that allows CORS
+	corsMux := middlewareCors(r)
 
 	// create server and listen
 	srv := http.Server{
 		Addr:    ":8080",
-		Handler: r,
+		Handler: corsMux,
 	}
 	srv.ListenAndServe()
 }
